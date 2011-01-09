8fc137499dc26cf10e497e36340d04a52ae9f0e2
Massimo Zaniboni <massimo.zaniboni@profitoss.com>
2011-01-10 00:42:23 +0100

    Removed bad comments.

--- app/controllers/cacheable_pages_controller.rb
+++ app/controllers/cacheable_pages_controller.rb
@@ -1,9 +1,3 @@
-# TODO mostra HELP del plugin con indicato come cambiare il parametro di configurazione, il setup VCL da fare e altre cose
-
-# TODO dovrei inserire un file di testo dentro al controller e cose del genere e devo imparare come fa Rails a inserire delle pagine semplici e cose del genere
-
-# TODO 
-
 class CacheablePagesController < ApplicationController
   def index
     if defined?(::REDMINE_CACHEABLE_PAGESS_MAX_AGE_IN_MINUTES)
